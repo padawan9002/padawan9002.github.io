c0e1a53441190ed3cbcd26bfb5e90e70ea282d2b
padawan9002 <padawan9002@gmail.com>
2015-02-16 13:22:35 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var ball = {};
+var paddles = {p1: {}, p2: {}};
 
 function setup() {
   // put setup code here
@@ -6,13 +7,16 @@ function setup() {
   ball.x = width/2;
   ball.y = height/2;
   ball.diameter = 20;
+  ball.speedX = 1;
+  ball.speedY = 1;
+  ball.draw = function (){ellipse(ball.x, ball.y, ball.diameter, ball.diameter);};
 }
 
 function draw() {
   // put drawing code here
-  ellipse(ball.x, ball.y, ball.diameter, ball.diameter);
-  ball.x += 1;
-  ball.y += 1;
+  ball.draw()
+  ball.x = ball.x + ball.speedX;
+  ball.y = ball.y + ball.speedY;
 }
 
 //Thanks http://lauren-mccarthy.com/about/
