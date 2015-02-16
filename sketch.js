9e52ca6be86d3202fc10b47f6de1530d7987ecad
padawan9002 <padawan9002@gmail.com>
2015-02-16 13:01:43 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 function setup() {
   // put setup code here
+  createCanvas(640, 480);
 }
 
 function draw() {
   // put drawing code here
   ellipse(50,50,50,50);
 }
+
+//Thanks http://lauren-mccarthy.com/about/
